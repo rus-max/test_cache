e79efcd45eb7792f284fe5e32925aab94e368ed0
Max Erkin <m.erkin@netology-group.ru>
2016-06-01 20:10:47 +0300

    cached_block even if config.perform_caching = false

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def some_good_cache_block(expires_time)
+    Rails.cache.fetch('some_good_cache_block', expires_in: expires_time) do
+      yield.html_safe if block_given?
+    end
+  end
 end
